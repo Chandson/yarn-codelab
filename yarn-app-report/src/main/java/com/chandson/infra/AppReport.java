9d524fdf2cf7028915dfa0a7e04303a37051c220
linyouquan <linyouquan@xiaomi.com>
2020-05-04 10:38:22 +0800

    Debug ClientToAMTokenIdentifier for AppReport

--- yarn-app-report/src/main/java/com/chandson/infra/AppReport.java
+++ yarn-app-report/src/main/java/com/chandson/infra/AppReport.java
@@ -1,6 +1,8 @@
 package  com.chandson.infra;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.net.NetUtils;
+import org.apache.hadoop.security.token.Token;
 import org.apache.hadoop.yarn.api.records.ApplicationId;
 import org.apache.hadoop.yarn.api.records.ApplicationReport;
 import org.apache.hadoop.yarn.api.records.YarnApplicationState;
@@ -9,9 +11,11 @@ import org.apache.hadoop.yarn.conf.YarnConfiguration;
 import org.apache.hadoop.yarn.exceptions.YarnException;
 
 import org.apache.hadoop.security.UserGroupInformation;
+import org.apache.hadoop.yarn.security.client.ClientToAMTokenIdentifier;
 import org.apache.hadoop.yarn.util.ConverterUtils;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -31,6 +35,17 @@ public class AppReport {
             for (ApplicationReport application: applications) {
                 System.out.println("appId: " + application.getApplicationId() + ", name: " + application.getName() +
                         ", queue: " + application.getQueue());
+
+                // debug ClientToAMTokenIdentifier
+                InetSocketAddress serviceAddr = null;
+                serviceAddr = NetUtils.createSocketAddrForHost(
+                        application.getHost(), application.getRpcPort());
+                Token<ClientToAMTokenIdentifier> token =
+                        ConverterUtils.convertFromYarn(application.getClientToAMToken(), serviceAddr);
+                System.out.println("attemptId: " + token.decodeIdentifier().getApplicationAttemptID());
+                System.out.println("client: " + token.decodeIdentifier().getClientName());
+                System.out.println("user: " + token.decodeIdentifier().getUser());
+                System.out.println("service: " + token.getService());
             }
         } catch (YarnException e) {
             e.printStackTrace();
